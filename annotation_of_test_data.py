e83b9c022725a2fa18fdd121552b58a2a75ab68b
BILGE ECE GUNDOGDU <ge83gux@mytum.de>
2024-05-22 00:53:21 +0200

    labeling annotations and topics ece

--- annotation_of_test_data.py
+++ annotation_of_test_data.py
@@ -36,17 +36,17 @@ annotations_1_118 = [1, -1, -1, -1, -1, -1, 1, 1, -1, 1,
 # labels for 119-238 (Niklas)
 # to get started
 
-annotations_119_238 = [1, 1, -1, 1, -1, -1, -1, 1, -1, 1, 1, -1, 
+annotations_119_238 = [1, 1, -1, 1, -1, -1, -1, 1, -1, 1, 1, -1,
                        -1, -1, -1, -1, -1, 1, 1, -1, -1, 1,
-                       -1, -1, -1, 0, -1, -1, 0, -1, 1, -1, 
-                       1, -1, -1, 0, 1, 1, 1, -1, 0, 0, 
+                       -1, -1, -1, 0, -1, -1, 0, -1, 1, -1,
+                       1, -1, -1, 0, 1, 1, 1, -1, 0, 0,
                        -1, -1, 1, 1, 1, -1, 0, -1, 1, 1,
-                       -1, 1, -1, -1, -1, 0, -1, -1,-1, 1,
+                       -1, 1, -1, -1, -1, 0, -1, -1, -1, 1,
                        -1, 1, -1, 1, 1, -1, -1, 1, -1, 0,
-                       -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 
+                       -1, 1, 1, 1, 1, 1, -1, 1, 1, -1,
                        -1, 1, -1, 0, 0, -1, 1, 0, -1, -1,
                        -1, -1, 1, -1, -1, -1, -1, 1, -1, -1,
-                       -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, 
+                       -1, -1, -1, 1, -1, -1, -1, 1, -1, -1,
                        1, -1, -1, 1, -1, -1, -1, -1]
 
 # My last review (included) "customer service is shocking"
@@ -55,11 +55,33 @@ annotations_119_238 = [1, 1, -1, 1, -1, -1, -1, 1, -1, 1, 1, -1,
 annotations_239_355 = [0, 0, 0,
                        0]
 
-# labels for 356-472 (Ece)
-annotations_356_472 = [0, 0, 0,
-                       0]
+# My frist review (included) "Ryanair Customer Review - The bad press surrounding Ryanair proved to be totally
+# unjustified on my round trip Madrid-Cagliari-Madrid. Check- in and security was the quickest and most polite ever (
+# especially at the dedicated check-in/security at Madrid. Boarding was orderly and uneventful (both in Madrid and
+# Cagliari). We left on schedule and arrived early on both segments. The flight attendants were professional polite
+# and friendly. No negative comments at all and I'll be only too happy to fly with Ryanair again."
 
-# My last review (included) "Never been a huge fan of Ryanair from past experiences but I acknowledge they provide an A-B service at a generally good price and that is why they are so popular. I wasn't expecting much from this recent trip I made from Stansted to Gothenburg but I came away feeling I'd do it all again if this is what I can get for my money. On time flights, clean airplane, friendly cabin crew and excellent choice of on board products. Yes, they are a hard seller and the rules are what they are, but we should all know that by now, and for the price you pay nothing comes close. If you want a weekend away in Europe at a low price why would you look elsewhere?"
+# labels for 356-472 (Ece)
+annotations_356_472 = [1, 1, 0, 1, -1, 1, 1, 1, -1, -1,
+                       1, -1, -1, -1, -1, 1, -1, -1, 1, 1,
+                       1, 1, 1, 1, -1, 1, 1, 1, 0, 1,
+                       -1, 1, 1, -1, 1, 1, 1, -1, 1, -1,
+                       -1, -1, -1, -1, 1, -1, -1, 0, 1, 1,
+                       1, -1, 1, -1, 1, 1, 1, -1, 1, -1,
+                       0, -1, -1, 0, -1, 1, 1, -1, 0, -1,
+                       1, -1, -1, -1, -1, -1, 1, -1, -1, -1,
+                       1, 0, 1, -1, 1, 1, -1, -1, -1, 1,
+                       1, -1, 1, 1, -1, -1, -1, 1, -1, -1,
+                       -1, -1, 1, 1, 1, 1, -1, -1, - 1, -1,
+                       -1, -1, 1, -1, 1, 0, 0]
+
+# My last review (included) "friendly cabin crew - Never been a huge fan of Ryanair from past experiences but I
+# acknowledge they provide an A-B service at a generally good price and that is why they are so popular. I wasn't
+# expecting much from this recent trip I made from Stansted to Gothenburg but I came away feeling I'd do it all again
+# if this is what I can get for my money. On time flights, clean airplane, friendly cabin crew and excellent choice
+# of on board products. Yes, they are a hard seller and the rules are what they are, but we should all know that by
+# now, and for the price you pay nothing comes close. If you want a weekend away in Europe at a low price why would
+# you look elsewhere?"
 
 # concat annotations
 # annotations = pd.concat([annotations_1_118, annotations_119_238, annotations_239_355, annotations_356_472])
