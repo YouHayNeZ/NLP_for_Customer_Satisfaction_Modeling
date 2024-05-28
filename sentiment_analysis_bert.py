cfa9232ba71b1072a5a0d6e01a3e542d9d246517
pati-sl <patrycja.slotosch@gmail.com>
2024-05-28 21:06:42 +0200

    bert sentiment analysis - siebert and distilbert

--- sentiment_analysis_bert.py
+++ sentiment_analysis_bert.py
@@ -6,20 +6,17 @@ def preprocess_text(text):
     text = text.lower()
     return text
 
+
 df = pd.read_csv('data/ryanair_reviews.csv')
 
-comment_title = df['Comment title'].apply(preprocess_text).tolist()
 comment = df['Comment'].apply(preprocess_text).tolist()
-sentiment_analyzer = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
-sentiment_comment_title = sentiment_analyzer(comment_title, truncation=True, max_length=512)
-sentiment_comment = sentiment_analyzer(comment, truncation=True, max_length=512)
 
-df['sentiment_comment_title'] = [result['label'] for result in sentiment_comment_title]
-df['sentiment_score_comment_title'] = [result['score'] for result in sentiment_comment_title]
+sentiment_analyzer_distilbert = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
+sentiment_comment_distilbert = sentiment_analyzer_distilbert(comment, truncation=True, max_length=512)
+df['sentiment_comment_distilbert'] = [result['label'] for result in sentiment_comment_distilbert]
 
-df['sentiment_comment'] = [result['label'] for result in sentiment_comment]
-df['sentiment_score_comment'] = [result['score'] for result in sentiment_comment]
+sentiment_analyzer_siebert = pipeline('sentiment-analysis', model='siebert/sentiment-roberta-large-english')
+sentiment_comment_siebert = sentiment_analyzer_distilbert(comment, truncation=True, max_length=512)
+df['sentiment_comment_siebert'] = [result['label'] for result in sentiment_comment_siebert]
 
 df.to_csv('data/ryanair_reviews_with_bert_sentiment.csv', index=False)
-
-print(df.head())
